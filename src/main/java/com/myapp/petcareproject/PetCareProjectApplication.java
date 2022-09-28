0c49b4ae45d21811cc4499d5837bb3461c2199b9
ILYA EGOROV <egorovcf@gmail.com>
2022-09-29 01:57:20 +0300

    Initial commit

---
+++ src/main/java/com/myapp/petcareproject/PetCareProjectApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.petcareproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetCareProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetCareProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myapp/petcareproject/PetCareProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.petcareproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetCareProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
